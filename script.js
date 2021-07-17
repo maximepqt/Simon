2e65f4339a24afc6c129c638fe2a75825bfbc283
Maxime PAQUET <maxime.paquet1245@aol.fr>
2021-07-17 20:12:49 +0200

    Add Radom Color + Random Number

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
 var largeur = window.innerWidth;
 var hauteur = window.innerHeight;
 
+let squareChange = document.getElementById("square")
+
 console.log(largeur);
 console.log(hauteur);
+
+let arrColor =[ 'black','red','yellow','blue','green','brown','purple','pink'];
+
+function randomColor(){
+    const randomNumberCase = Math.floor(Math.random() * 50) + 1
+    console.log(randomNumberCase);
+    const randomNumberColor = Math.floor(Math.random() * 7) + 1
+    console.log(randomNumberColor);
+
+
+    let arrColorPicker = arrColor[randomNumberColor];
+    let squareCasePicker = "square" + randomNumberCase;
+    let squareChange = document.getElementById("square")
+
+    squareChange.style.backgroundColor= arrColorPicker;
+
+    console.log(arrColorPicker);
+
+    console.log(squareCasePicker);
+}
+
+randomColor();
