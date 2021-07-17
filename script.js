8a53ceca7e7250afa9c8e83333f14d7810271803
Maxime PAQUET <maxime.paquet1245@aol.fr>
2021-07-17 20:21:37 +0200

    Add Color

--- script.js
+++ script.js
@@ -6,12 +6,12 @@ var hauteur = window.innerHeight;
 console.log(largeur);
 console.log(hauteur);
 
-let arrColor =[ 'black','red','yellow','blue','green','brown','purple','pink'];
+let arrColor =[ 'black','red','yellow','blue','green','brown','purple','pink','#4287f5','#4287f5','#b0f542','#8a42f5','#e8486d','#757575','#9c2424','#78ad61'];
 
 function randomColor(){
     const randomNumberCase = Math.floor(Math.random() * 50) + 1
     console.log(randomNumberCase);
-    const randomNumberColor = Math.floor(Math.random() * 7) + 1
+    const randomNumberColor = Math.floor(Math.random() * arrColor.length) + 1
     console.log(randomNumberColor);
 
 
